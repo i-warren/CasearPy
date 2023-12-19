d72d843da5762a84945c14c1e09f00858652092f
i-warren <102181233+i-warren@users.noreply.github.com>
2023-12-19 15:42:00 -0500

    Add files via upload
    Revised shift modulo to not rely on if statement

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ import art
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 print(art.logo)
-
 run = "yes"
-
-
 
 def caesar(start_text, shift_amount, code_direction):
 
@@ -41,8 +38,8 @@ while run == "yes":
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
-    if shift > 26:
-        shift %= 26
+    shift %= 26
+
     caesar(start_text=text, shift_amount=shift, code_direction=direction)
 
     run = input("Would you like to run the program again? (yes or no):\n").lower()
